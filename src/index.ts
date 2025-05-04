b8f1a4df8166e8e5419a78c81ecec75fe00552c2
Dustin <dustin@scorecard.io>
2025-05-04 12:55:18 -0700

    OIDC WIP.

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,69 @@
-import { McpAgent } from "agents/mcp";
-import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { init, server } from "scorecard-ai-mcp/server";
 import Scorecard from "scorecard-ai";
-import { z } from "zod";
+import { Hono } from "hono";
+import { McpAgent } from "agents/mcp";
+// import { DurableMCP } from "workers-mcp"; Not even a valid import?
+//import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import OAuthProvider, { type OAuthHelpers } from "@cloudflare/workers-oauth-provider";
 
-// Define environment interface (required to use this.env)
-interface Env {
-	SCORECARD_API_KEY: string;
-}
+import type { UserProps } from "./types";
+import { authorize, callback, confirmConsent, tokenExchangeCallback } from "./auth";
 
-// Define our MCP agent with tools
-export class MyMCP extends McpAgent<Env> {
+export class AuthenticatedMCP extends McpAgent<UserProps, Env> {
 
 	server = server;
 
 	async init() {
+		// Add additional tool for debugging. This will show the current user's claims and the Auth0 tokens.
+		server.tool("whoami", "Get the current user's details", {}, async () => ({
+			content: [{ type: "text", text: JSON.stringify(this.props.claims, null, 2) }],
+		}));
+
+
 		const client = new Scorecard({
-			bearerToken: this.env.SCORECARD_API_KEY
+			// TODO: This @ts-ignore shouldn't be required, the type is defined in UserProps
+			// @ts-ignore
+			bearerToken: this.props.tokenSet.accessToken,
 		})
 
 		init({ server: this.server, client});
 	}
 }
 
-export default {
-	fetch(request: Request, env: Env, ctx: ExecutionContext) {
-		const url = new URL(request.url);
+// export default {
+// 	fetch(request: Request, env: Env, ctx: ExecutionContext) {
+// 		const url = new URL(request.url);
 
-		if (url.pathname === "/sse" || url.pathname === "/sse/message") {
-			// @ts-ignore
-			return MyMCP.serveSSE("/sse").fetch(request, env, ctx);
-		}
+// 		if (url.pathname === "/sse" || url.pathname === "/sse/message") {
+// 			// @ts-ignore
+// 			return MyMCP.serveSSE("/sse").fetch(request, env, ctx);
+// 		}
 
-		if (url.pathname === "/mcp") {
-			// @ts-ignore
-			return MyMCP.serve("/mcp").fetch(request, env, ctx);
-		}
+// 		if (url.pathname === "/mcp") {
+// 			// @ts-ignore
+// 			return MyMCP.serve("/mcp").fetch(request, env, ctx);
+// 		}
 
-		return new Response("Not found", { status: 404 });
-	},
-};
+// 		return new Response("Not found", { status: 404 });
+// 	},
+// };
+
+// Initialize the Hono app with the routes for the OAuth Provider.
+const app = new Hono<{ Bindings: Env & { OAUTH_PROVIDER: OAuthHelpers } }>();
+app.get("/authorize", authorize);
+app.post("/authorize/consent", confirmConsent);
+app.get("/callback", callback);
+
+export default new OAuthProvider({
+	apiRoute: "/mcp",
+	// TODO: fix these types
+	// @ts-ignore
+	apiHandler: AuthenticatedMCP.mount("/mcp"),
+	// TODO: fix these types
+	// @ts-ignore
+	defaultHandler: app,
+	authorizeEndpoint: "/authorize",
+	tokenEndpoint: "/token",
+	clientRegistrationEndpoint: "/register",
+	tokenExchangeCallback,
+});
